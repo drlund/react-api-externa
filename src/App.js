1b3d7686e7b308ee51c7c0b740d979ca0ddd5ae0
Paulo Cesar Souza Pereira <drlund@hotmail.com>
2022-05-15 18:36:38 -0300

    Implementação de cosumo de API externa em React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import api from './api';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    usuarios: [],
+  }
+
+  async componentDidMount() {
+    const response = await api.get('');
+
+    this.setState({ usuarios: response.data });
+}
+
+  render() {
+
+    const { usuarios } = this.state;
+
+    return(
+      <div>
+        <h1>Listar Usuarios</h1>
+        { usuarios.map(usuario =>(
+          <li key={usuario.company.id}>
+            <h2><strong>Usuário: </strong>
+            {usuario.company.name} </h2>
+            <p>{usuario.company.catchPhrase}</p>
+          </li>
+        ))}
+      </div>
+    )
+  }
 }
 
 export default App;
